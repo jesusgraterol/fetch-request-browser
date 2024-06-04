5747feecafd64f8e4e359052a23646f7c9f8f9e7
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-06-04 13:14:47 -0400

    Implement the integration tests for the sendGET and sendPOST functions
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.test-integration.ts
+++ src/index.test-integration.ts
@@ -1,4 +1,5 @@
-import { describe, beforeAll, afterAll, beforeEach, afterEach, test, expect, vi } from 'vitest';
+import { describe, beforeAll, afterAll, beforeEach, afterEach, test, expect } from 'vitest';
+import { sendGET, sendPOST } from './index.js';
 
 /* ************************************************************************************************
  *                                             TESTS                                              *
@@ -13,7 +14,62 @@ describe('sendGET', () => {
 
   afterEach(() => { });
 
-  test('can send a GET request', () => {
-    expect(2 + 2).toBe(4);
-  });
+  test('can send a GET request', async () => {
+    const url = 'https://httpbin.org/get';
+    const { headers, data } = await sendGET(url);
+
+    expect(headers.get('Content-Type')).toBe('application/json');
+
+    expect(typeof data).toBe('object');
+    expect(data.args).toStrictEqual({});
+    expect(typeof data.headers).toBe('object');
+    expect(data.headers.Accept).toBe('application/json');
+    expect(data.headers['Content-Type']).toBe('application/json');
+    expect(data.url).toBe(url);
+  }, 60000);
+
+
+  test('can send a GET request w/ query string', async () => {
+    const url = 'https://httpbin.org/get?foo=hey&bar=123';
+    const { headers, data } = await sendGET(url);
+
+    expect(headers.get('Content-Type')).toBe('application/json');
+
+    expect(typeof data).toBe('object');
+    expect(data.args).toStrictEqual({ foo: 'hey', bar: '123'});
+    expect(typeof data.headers).toBe('object');
+    expect(data.headers.Accept).toBe('application/json');
+    expect(data.headers['Content-Type']).toBe('application/json');
+    expect(data.url).toBe(url);
+  }, 60000);
+});
+
+
+describe('sendPOST', () => {
+  beforeAll(() => { });
+
+  afterAll(() => { });
+
+  beforeEach(() => { });
+
+  afterEach(() => { });
+
+  test('can send a POST request with a body', async () => {
+    const url = 'https://httpbin.org/post';
+    const body = {
+      someKey: 'Hello',
+      someNumber: 123456,
+    };
+    const { headers, data } = await sendPOST(url, { requestOptions: { body } });
+
+    expect(headers.get('Content-Type')).toBe('application/json');
+
+    expect(typeof data).toBe('object');
+    expect(data.args).toStrictEqual({});
+    expect(data.json).toStrictEqual(body);
+    expect(typeof data.headers).toBe('object');
+    expect(data.headers.Accept).toBe('application/json');
+    expect(data.headers['Content-Type']).toBe('application/json');
+    expect(data.url).toBe(url);
+  }, 60000);
 });
