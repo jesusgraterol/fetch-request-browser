4cff5a9632bdb5a90eecada8b0314e847f73001b
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-06-13 18:06:20 -0400

    Implement the remaining unit tests
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/validations/validation.test-unit.ts
+++ src/validations/validation.test-unit.ts
@@ -48,6 +48,12 @@ describe('validateResponse', () => {
     ).toThrowError(ERRORS.UNEXPECTED_RESPONSE_STATUS_CODE);
   });
 
+  test('throws if the response doesn\'t have the Content-Type Header', () => {
+    expect(
+      () => validateResponse(rq(), rs(200, 'OK', new Headers()), buildOptions()),
+    ).toThrowError(ERRORS.INVALID_RESPONSE_CONTENT_TYPE);
+  });
+
   test('a response can be successful even if the Content-Type Header is not identical to the request\'s Accept Header', () => {
     expect(
       validateResponse(rq(), rs(200, 'OK', new Headers({ 'Content-Type': 'application/json; charset=utf-8' })), buildOptions()),
