88d8ff3b1bacebf26a149e2c1ce202087d66c66c
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2025-05-18 09:52:30 -0400

    Refactor the __executeSend func so it attempts to extract the error from the body in case the fetch fails
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import {
 import {
   buildOptions,
   buildRequest,
+  extractErrorMessageFromResponseBody,
   extractResponseData,
 } from './utils/utils.js';
 import { validateResponse } from './validations/validations.js';
@@ -41,20 +42,31 @@ const __executeSend = async <T>(
   // send the request
   const res = await fetch(req);
 
-  // validate the response
-  validateResponse(req, res, opts);
+  // if the validation succeeds, build the response. Otherwise, try to extract the cause of the
+  // error from the body of the response
+  try {
+    // validate the response
+    validateResponse(req, res, opts);
 
-  // print a warning in case the request was redirected
-  if (res.redirected) {
-    console.warn(`The request sent to '${req.url}' was redirected. Please update the implementation to avoid future redirections.`);
+    // print a warning in case the request was redirected
+    if (res.redirected) {
+      console.warn(`The request sent to '${req.url}' was redirected. Please update the implementation to avoid future redirections.`);
+    }
+
+    // return the request's response
+    return {
+      code: res.status,
+      statusText: res.statusText,
+      headers: res.headers,
+      data: await extractResponseData<T>(res, opts.responseDataType),
+    };
+  } catch (e) {
+    const cause = await extractErrorMessageFromResponseBody(res);
+    if (cause) {
+      throw new Error(extractMessage(e), { cause });
+    }
+    throw e;
   }
-
-  // return the request's response
-  return {
-    code: res.status,
-    headers: res.headers,
-    data: await extractResponseData<T>(res, opts.responseDataType),
-  };
 };
 
 /**
